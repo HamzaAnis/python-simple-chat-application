524aa4f6b0cb821409d31221c056e74a1d57f555
Hamza Anis <hamzaanis9514@gmail.com>
2018-06-06 04:36:16 +0500

    Registration send ✔

--- UdpChat/UpdChat.py
+++ UdpChat/UpdChat.py
@@ -3,18 +3,42 @@ from logging.config import fileConfig
 from os import path
 import os
 import sys
+import socket
+
 class Server(object):
     """docstring for Server."""
+
     def __init__(self, port):
         super(Server, self).__init__()
-        self.port=port
-        print("Server started")
+        self.port = port
+
+    def start(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind(('', int(self.port)))
+        logging.info("Server started")
+        while True:
+            message, address = self.server_socket.recvfrom(1024)
+            logging.info(message) 
+   
+
+class Client(object):
+    """docstring for Client."""
+
+    def __init__(self, nickname, server_ip, server_port, client_port):
+        super(Client, self).__init__()
+        self.nick_name=nickname
+        self.server_ip=server_ip
+        self.server_port=server_port
+        self.client_port=client_port
+    def start(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client_socket.settimeout(1.0)
+        self.addr = (self.server_ip, int(self.server_port))
+        reg=self.nick_name+" "+self.client_port+" "+self.server_ip+" "+self.server_port
+        self.client_socket.sendto(reg, self.addr)
+        logging.info("Client reg send")
 
 
-    def start_server(self):
-        print("Server started")
-        
-        
 class UdpChat(object):
     """docstring for UdpChat."""
 
@@ -30,9 +54,10 @@ class UdpChat(object):
         self.mode = mode
         if (mode == "-c"):
             logging.info("Client Called")
+            self.instance=Client(nick_name,server_ip,server_port,client_port)
         elif (mode == "-s"):
             logging.info("Server Called")
-            self.instance=Server(port)
+            self.instance = Server(port)
 
 
 if __name__ == "__main__":
@@ -46,3 +71,8 @@ if __name__ == "__main__":
     elif (len(sys.argv) == 6):
         U = UdpChat(sys.argv[1], None, sys.argv[2], sys.argv[3], sys.argv[4],
                     sys.argv[5])
+    else:
+        logging.critical("Invalid format!")
+        print("Exiting")
+        sys.exit(0)
+    U.instance.start()
