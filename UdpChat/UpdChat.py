6548219550fb836add7c09600e0f9c96855d2ff7
Hamza Anis <hamzaanis9514@gmail.com>
2018-06-06 05:28:20 +0500

    Client table received

--- UdpChat/UpdChat.py
+++ UdpChat/UpdChat.py
@@ -24,18 +24,33 @@ class Server(object):
             logging.info(message)
             self.server_socket.sendto("Welcome, You are registered.".encode(),
                                       address)
+            # client information received from client
             client_data = str(message).split(" ")
+            # appending to the client table
+            self.client_table.append(client_data)
             logging.info("Client Port is " + client_data[1])
             logging.info(client_data)
-            self.client_table_broadcast_service(client_data[2],int(client_data[1]))
+            self.client_table_broadcast_service(client_data[2],
+                                                int(client_data[1]))
 
     def client_table_broadcast_service(self, client_ip, client_port):
+        """To send the client table on the client's port
+        
+        Arguments:
+            client_ip {[str]} -- [Client Ip]
+            client_port {[int]} -- [Client Port]
+        """
+
         broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         b_addr = (client_ip, client_port)
-        logging.info("Sent to "+client_ip+"  "+str(client_port))
-        broadcast_socket.sendto("Here is your client table".encode(), b_addr)
-
-
+        logging.info("Sent to " + client_ip + "  " + str(client_port))
+        broadcast_socket.sendto(self.table_to_string().encode(), b_addr)
+    
+    def table_to_string(self):
+        send=""
+        for v in self.client_table:
+            send=send+v[0]+" "+v[1]+" "+v[2]+" "+v[3]+"\n"
+        return send
 class Client(object):
     """docstring for Client."""
 
@@ -54,21 +69,25 @@ class Client(object):
             group=None,
             target=self.client_table_broadcast_service,
             name="Broadcast Service")
+        # starting broadcast thread to recieve client table from server
         self.broadcast_thread.start()
+        # starting client initial registration
         self.do_registration()
         # to start the thread to receive the table
-       
 
     def client_table_broadcast_service(self):
-        logging.info("Client table service started at "+self.client_port)
+        """This method starts another socket on which it receives the update client table
+        """
+
+        logging.info("Client table service started at " + self.client_port)
         self.broadcast_socket = socket.socket(socket.AF_INET,
                                               socket.SOCK_DGRAM)
         self.broadcast_socket.bind(('', int(self.client_port)))
         while True:
             message, address = self.broadcast_socket.recvfrom(1024)
             print("[Client table received]\n" + message)
-            logging.info("Client table service received at "+self.client_port)
-
+            logging.info("Client table service received at " +
+                         self.client_port)
 
     def do_registration(self):
         reg = self.nick_name + " " + self.client_port + " " + self.server_ip + " " + self.server_port
